b69d334f1458ef99dfa73ab330c786c55c1e1b97
FelipeCGomes <75631219+FelipeCGomes@users.noreply.github.com>
2026-08-30 19:40:50 -0300

    config: connect Stop Gastos Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -2,17 +2,18 @@
   'use strict';
 
   // Configuração pública do app Web do Firebase.
-  // Preencha os valores do Firebase Console > Configurações do projeto > Seus apps > Web.
   // Não coloque serviceAccount, private keys ou segredos neste arquivo.
   root.STOP_GASTOS_FIREBASE_CONFIG = {
-    apiKey: '',
-    authDomain: '',
-    projectId: '',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: ''
+    apiKey: 'AIzaSyCg9hI4SY9Uo1vrwdLSq-liiuGtgI0nmD8',
+    authDomain: 'stopgastos.firebaseapp.com',
+    projectId: 'stopgastos',
+    storageBucket: 'stopgastos.firebasestorage.app',
+    messagingSenderId: '363408500943',
+    appId: '1:363408500943:web:aa4d5ec09bb575fc2f2fba',
+    measurementId: 'G-474SM3X55N'
   };
 
   // Firebase Console > Cloud Messaging > Web Push certificates.
+  // Preencha apenas com a chave pública VAPID quando ela for gerada.
   root.STOP_GASTOS_FIREBASE_VAPID_KEY = '';
 })(globalThis);
